cabd988aeec6d1694a660d9d3b13be8e878b8b2f
ramiro <leporiramiro@gmail.com>
2022-03-10 19:41:23 -0300

    Porfolio5

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,86 +5,81 @@ const CopyPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-    // mode: 'production', // LE INDICO EL MODO EXPLICITAMENTE
-    entry: './src/index.js', // el punto de entrada de mi aplicación
-    output: { // Esta es la salida de mi bundle
-        path: path.resolve(__dirname, 'dist'),
-        // resolve lo que hace es darnos la ruta absoluta de el S.O hasta nuestro archivo
-        // para no tener conflictos entre Linux, Windows, etc
-        filename: 'main.js', 
-        // EL NOMBRE DEL ARCHIVO FINAL,
-        assetModuleFilename: 'assets/images/[hash][ext][query]'
-    },
-    mode: 'development',
-    watch: true,
-    resolve: {
-        extensions: ['.js'], // LOS ARCHIVOS QUE WEBPACK VA A LEER
-        alias: {
-            '@utils': path.resolve(__dirname, 'src/utils/'),
-            '@templates': path.resolve(__dirname, 'src/templates/'),
-            '@styles': path.resolve(__dirname, 'src/styles/'),
-            '@images': path.resolve(__dirname, 'src/assets/images/'),
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/images/[hash][ext][query]'
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@utils': path.resolve(__dirname, 'src/utils/'),
+      '@templates': path.resolve(__dirname, 'src/templates/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@images': path.resolve(__dirname, 'src/assets/images/'),
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
         }
-    },
-    module: {
-        // REGLAS PARA TRABAJAR CON WEBPACK
-        rules : [
-            {
-                test: /\.m?js$/, // LEE LOS ARCHIVOS CON EXTENSION .JS,
-                exclude: /node_modules/, // IGNORA LOS MODULOS DE LA CARPETA
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.css|.styl$/i,
-                use: [ MiniCssExtractPlugin.loader, 'css-loader' , 'stylus-loader' ]
-            },
-            {
-                test: /\.png/, // REGLA PARA ACEPTAR IMAGENES .PNG
-                type: 'asset/resource'
-            },
-            {
-                test: /\.(woff|woff2)$/, // REGLA PARA ARCHIVOS WOFF | WOFF2
-                use: {
-                    loader: 'url-loader', // NOMBRE DEL LOADER
-                    options: {
-                        limit: false, // O LE PASAMOS UN NUMERO
-                        // Habilita o deshabilita la transformación de archivos en base64.
-                        mimetype: 'aplication/font-woff',
-                        // Especifica el tipo MIME con el que se alineará el archivo. 
-                        // Los MIME Types (Multipurpose Internet Mail Extensions)
-                        // son la manera standard de mandar contenido a través de la red.
-                        name: "[name].[ext]",
-                        // EL NOMBRE INICIAL DEL PROYECTO + SU EXTENSIÓN
-                        // PUEDES AGREGARLE [name]hola.[ext] y el output del archivo seria 
-                        // ubuntu-regularhola.woff
-                        outputPath: './assets/fonts/', 
-                        // EL DIRECTORIO DE SALIDA (SIN COMPLICACIONES)
-                        publicPath: '../assets/fonts/',
-                        // EL DIRECTORIO PUBLICO (SIN COMPLICACIONES)
-                        esModule: false
-                    }
-                }
-            }
-        ]
-    },
-    // SECCION DE PLUGINS
-    plugins: [
-        new HtmlWebpackPlugin({ // CONFIGURACIÓN DEL PLUGIN
-            inject: true, // INYECTA EL BUNDLE AL TEMPLATE HTML
-            template: './public/index.html', // LA RUTA AL TEMPLATE HTML
-            filename: './index.html' // NOMBRE FINAL DEL ARCHIVO
-        }),
-        new MiniCssExtractPlugin(), // INSTANCIAMOS EL PLUGIN
-        new CopyPlugin({ // CONFIGURACIÓN DEL COPY PLUGIN
-            patterns: [
-                {
-                    from: path.resolve(__dirname , "src" , 'assets/images'), // CARPETA A MOVER AL DIST
-                    to: "assets/images" // RUTA FINAL DEL DIST
-                }
-            ]
-        }),
-        new Dotenv(),
+      },
+      {
+        test: /\.css|.styl$/i,
+        use: [MiniCssExtractPlugin.loader,
+          'css-loader',
+          'stylus-loader'
+        ],
+      },
+      {
+        test: /\.png/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            mimetype: "application/font-woff",
+            name: "[name].[contenthash].[ext]",
+            outputPath: "./assets/fonts/",
+            publicPath: "../assets/fonts/",
+            esModule: false,
+          },
+        }
+      }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './public/index.html',
+      filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].[contenthash].css'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets/images"),
+          to: "assets/images"
+        }
+      ]
+    }),
+    new Dotenv(),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: true,
+    port: 3006,
+  },
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,100 +2,88 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
-const TerserPlugin = require('terser-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    // mode: 'production', // LE INDICO EL MODO EXPLICITAMENTE
-    entry: './src/index.js', // el punto de entrada de mi aplicación
-    output: { // Esta es la salida de mi bundle
-        path: path.resolve(__dirname, 'dist'),
-        // resolve lo que hace es darnos la ruta absoluta de el S.O hasta nuestro archivo
-        // para no tener conflictos entre Linux, Windows, etc
-        filename: '[name].[contenthash].js', 
-        // EL NOMBRE DEL ARCHIVO FINAL,
-        assetModuleFilename: 'assets/images/[hash][ext][query]'
-    },
-    resolve: {
-        extensions: ['.js'], // LOS ARCHIVOS QUE WEBPACK VA A LEER
-        alias: {
-            '@utils': path.resolve(__dirname, 'src/utils/'),
-            '@templates': path.resolve(__dirname, 'src/templates/'),
-            '@styles': path.resolve(__dirname, 'src/styles/'),
-            '@images': path.resolve(__dirname, 'src/assets/images/'),
-        }
-    },
-    module: {
-        // REGLAS PARA TRABAJAR CON WEBPACK
-        rules : [
-            {
-                test: /\.m?js$/, // LEE LOS ARCHIVOS CON EXTENSION .JS,
-                exclude: /node_modules/, // IGNORA LOS MODULOS DE LA CARPETA
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.css|.styl$/i,
-                use: [ MiniCssExtractPlugin.loader, 'css-loader' , 'stylus-loader' ]
-            },
-            {
-                test: /\.png/, // REGLA PARA ACEPTAR IMAGENES .PNG
-                type: 'asset/resource'
-            },
-            {
-                test: /\.(woff|woff2)$/, // REGLA PARA ARCHIVOS WOFF | WOFF2
-                use: {
-                    loader: 'url-loader', // NOMBRE DEL LOADER
-                    options: {
-                        limit: false, // O LE PASAMOS UN NUMERO
-                        // Habilita o deshabilita la transformación de archivos en base64.
-                        mimetype: 'aplication/font-woff',
-                        // Especifica el tipo MIME con el que se alineará el archivo. 
-                        // Los MIME Types (Multipurpose Internet Mail Extensions)
-                        // son la manera standard de mandar contenido a través de la red.
-                        name: "[name].[contenthash].[ext]",
-                        // EL NOMBRE INICIAL DEL PROYECTO + SU EXTENSIÓN
-                        // PUEDES AGREGARLE [name]hola.[ext] y el output del archivo seria 
-                        // ubuntu-regularhola.woff
-                        outputPath: './assets/fonts/', 
-                        // EL DIRECTORIO DE SALIDA (SIN COMPLICACIONES)
-                        publicPath: '../assets/fonts/',
-                        // EL DIRECTORIO PUBLICO (SIN COMPLICACIONES)
-                        esModule: false
-                    }
-                }
-            }
-        ]
-    },
-    // SECCION DE PLUGINS
-    plugins: [
-        new HtmlWebpackPlugin({ // CONFIGURACIÓN DEL PLUGIN
-            inject: true, // INYECTA EL BUNDLE AL TEMPLATE HTML
-            template: './public/index.html', // LA RUTA AL TEMPLATE HTML
-            filename: './index.html' // NOMBRE FINAL DEL ARCHIVO
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'assets/[name].[contenthash].css'
-        }), // INSTANCIAMOS EL PLUGIN
-        new CopyPlugin({ // CONFIGURACIÓN DEL COPY PLUGIN
-            patterns: [
-                {
-                    from: path.resolve(__dirname , "src" , 'assets/images'), // CARPETA A MOVER AL DIST
-                    to: "assets/images" // RUTA FINAL DEL DIST
-                }
-            ]
-        }),
-        new Dotenv(),
-        new CleanWebpackPlugin()
-    ],
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserPlugin(),
-        ]
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/images/[hash][ext][query]'
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@utils': path.resolve(__dirname, 'src/utils/'),
+      '@templates': path.resolve(__dirname, 'src/templates/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@images': path.resolve(__dirname, 'src/assets/images/'),
     }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css|.styl$/i,
+        use: [MiniCssExtractPlugin.loader,
+          'css-loader',
+          'stylus-loader'
+        ],
+      },
+      {
+        test: /\.png/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            mimetype: "application/font-woff",
+            name: "[name].[contenthash].[ext]",
+            outputPath: "./assets/fonts/",
+            publicPath: "../assets/fonts/",
+            esModule: false,
+          },
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './public/index.html',
+      filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].[contenthash].css'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets/images"),
+          to: "assets/images"
+        }
+      ]
+    }),
+    new Dotenv(),
+    new CleanWebpackPlugin(),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin(),
+    ]
+  }
 }
